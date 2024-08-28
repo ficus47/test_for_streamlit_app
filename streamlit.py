b8d161d456214e6e7bb7a396f5338d40e81c6733
ficus47 <140836421+ficus47@users.noreply.github.com>
2024-08-28 22:59:17 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -9,3 +9,4 @@ st.set_page_config(theme={"primaryColor":"#120ca0",
                           "backgroundColor":"#0d6364",
                           "secondaryBackgroundColor":"#2d0431",
                           "textColor":"#fbd1d1"}
+                  )
