b1373d5b1622c5e5dffc840f84fb35fa607daccd
ficus47 <140836421+ficus47@users.noreply.github.com>
2024-08-28 23:16:32 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,14 +1,26 @@
 import streamlit as st
 
-#try:
-#    open(".streamlit/config.toml", "a").write(open("theme.txt", 'r').read())
-#except Exception as e:
-#    st.write(e)
+import os
 
-#st.set_page_config()
-theme={"primaryColor":"#120ca0",
-                          "backgroundColor":"#0d6364",
-                          "secondaryBackgroundColor":"#2d0431",
-                          "textColor":"#fbd1d1"}
-                  #)
-st._config.set_option("theme.primaryColor", "#120ca0")
+# Dossier .streamlit et fichier config.toml
+config_dir = ".streamlit"
+config_file = "config.toml"
+
+# Crée le dossier .streamlit s'il n'existe pas
+if not os.path.exists(config_dir):
+    os.makedirs(config_dir)
+
+# Contenu du fichier config.toml
+config_content = """
+[theme]
+primaryColor = "#120ca0"
+backgroundColor = "#0d6364"
+secondaryBackgroundColor = "#2d0431"
+textColor = "#fbd1d1"
+"""
+
+# Écrit le contenu dans le fichier config.toml
+with open(os.path.join(config_dir, config_file), "w") as file:
+    file.write(config_content)
+
+st.write(f"Le fichier {config_file} a été créé/modifié avec succès.")
