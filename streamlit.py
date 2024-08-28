e9ee2742016f162d54d77238186924271151489a
ficus47 <140836421+ficus47@users.noreply.github.com>
2024-08-28 22:49:45 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,12 +1,6 @@
 import streamlit as st
 
 try:
-    open(".streamlit/config.toml", "w").write("""
-    [theme]
-    primaryColor="#120ca0"
-    backgroundColor="#0d6364"
-    secondaryBackgroundColor="#2d0431"
-    textColor="#fbd1d1"
-""")
+    open(".streamlit/config.toml", "w").write(open("theme.txt", 'r').read())
 except Exception as e:
     st.write(e)
