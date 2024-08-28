c34a41231026194372f17ecba88840f267e89f05
ficus47 <140836421+ficus47@users.noreply.github.com>
2024-08-28 22:58:20 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,6 +1,11 @@
 import streamlit as st
 
-try:
-    open(".streamlit/config.toml", "a").write(open("theme.txt", 'r').read())
-except Exception as e:
-    st.write(e)
+#try:
+#    open(".streamlit/config.toml", "a").write(open("theme.txt", 'r').read())
+#except Exception as e:
+#    st.write(e)
+
+st.set_page_config(theme={"primaryColor":"#120ca0",
+                          "backgroundColor":"#0d6364",
+                          "secondaryBackgroundColor":"#2d0431",
+                          "textColor":"#fbd1d1"}
